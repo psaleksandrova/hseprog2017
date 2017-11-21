7fa1aa32f38133c78f594e1c58a305b3bd10608f
psaleksandrova <31880931+psaleksandrova@users.noreply.github.com>
2017-11-21 15:30:55 +0300

    Update task5.py

--- bonus/task5.py
+++ bonus/task5.py
@@ -1,7 +1,4 @@
-consonants = ['B', 'b', 'C', 'c', 'D', 'd', 'F', 'f', 'G', 'g', 'H', 'h', 'J',
-              'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'P', 'p', 'Q', 'q',
-              'R', 'r', 'S', 's', 'T', 't', 'V', 'v', 'W', 'w', 'X', 'x', 'Y',
-              'y', 'Z', 'z']
+consonants = 'BbCcDdFfGgHhJjKkLlMmNnPpQqRrSsTtVvWwXxYyZz'
 st = input('Введите фразу: ')
 for i in range(len(st)):
     print(st[i], end = '')
