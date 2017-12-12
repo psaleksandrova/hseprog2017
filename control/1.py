f725c0569d4ac8421d2e5160f871769c9d53f352
psaleksandrova <31880931+psaleksandrova@users.noreply.github.com>
2017-12-12 13:22:04 +0300

    Update 1.py

--- control/1.py
+++ control/1.py
@@ -2,4 +2,4 @@ with open("Ozhegov.txt", encoding="utf-8") as f:
     for line in f:
         words = line.split('|')
         if len(words[0]) >= 20:
-            print(line)
+            print(line, end = '')
